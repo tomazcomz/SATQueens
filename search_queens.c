cecf69f8722a5014269f0d39b7ef11fb34b0824f
tomazcomz <comz@tutanota.com>
2025-01-13 02:16:12 +0000

    delivery

--- search_queens.c
+++ search_queens.c
@@ -61,22 +61,43 @@ int main(){
     int n;
     printf("How many Queens? \n");
     scanf("%d",&n);
-    printf("Performing DFS in %d Queens\n",n);
-    time_t begin = time(NULL);
-    Queens* s=emptyQueens(n);
-    Node* root = (Node*)malloc(sizeof(Node));
-    root->state = s;
-    root->children=(Node**)malloc(sizeof(Node*)*n);
-    for (int i = 0; i < s->n; i++){
-        root->children[i]=NULL;
+    if (n==0){
+        for (int a = 4; a < 22; a++)
+        {
+            time_t begin = time(NULL);
+            Queens* s=emptyQueens(a);
+            Node* root = (Node*)malloc(sizeof(Node));
+            root->state = s;
+            root->children=(Node**)malloc(sizeof(Node*)*a);
+            for (int i = 0; i < s->n; i++){
+                root->children[i]=NULL;
+            }
+            bool res=base_dfs(root);
+            if (!res)
+                printf("No solution found.");
+            time_t end = time(NULL);
+            long double time_spent = difftime(end, begin);
+            printf("%Lf\n",time_spent);
+            freeNode(root);
+        }
     }
-    bool res=base_dfs(root);
-    if (!res)
-        printf("No solution found.");
-    time_t end = time(NULL);
-    double time_spent = difftime(end, begin);
-    printf("Took %d seconds to find a solution", time_spent);
-    freeNode(root);
+    else if (n<=20) {
+        printf("Performing DFS in %d Queens\n",n);
+        time_t begin = time(NULL);
+        Queens* s=emptyQueens(n);
+        Node* root = (Node*)malloc(sizeof(Node));
+        root->state = s;
+        root->children=(Node**)malloc(sizeof(Node*)*n);
+        for (int i = 0; i < s->n; i++){
+            root->children[i]=NULL;
+        }
+        bool res=base_dfs(root);
+        if (!res)
+            printf("No solution found.");
+        time_t end = time(NULL);
+        long double time_spent = difftime(end, begin);
+        printf("Took %Lf seconds to find a solution", time_spent);
+    }
     return 0;
 }
 
